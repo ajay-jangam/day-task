37c7f3086fe92bab860d491c4d7525df09e5b4ca
Ajay <ajay.3mindsdigital@gmail.com>
2023-10-25 03:02:56 +0530

    Added authentication components

---
+++ src/components/input/Input.js
@@ -0,0 +1,62 @@
+import React from "react"
+import "./input.css"
+import CheckedIcon from "../../assets/images/icons/checkboxIcon.svg"
+import UncheckedIcon from "../../assets/images/icons/unchecked.svg"
+
+const Input = (props) => {
+	const {
+		id,
+		type,
+		label,
+		style,
+		onChange,
+		placeholder,
+		inputClass,
+		iconLeftUrl,
+		iconLeftAlt,
+		iconRightUrl,
+		iconRightAlt,
+		checkboxStatus,
+	} = props
+
+	return (
+		<>
+			<div className={inputClass}>
+				{iconLeftUrl && (
+					<img
+						className='inputIconLeft'
+						src={iconLeftUrl}
+						alt={iconLeftAlt}
+					/>
+				)}
+
+				{type === "checkbox" && (
+					<img
+						className='inputIconLeft'
+						src={checkboxStatus ? CheckedIcon : UncheckedIcon}
+						alt='checkbox'
+					/>
+				)}
+
+				<label htmlFor={id}>{label}</label>
+				<input
+					id={id}
+					type={type}
+					style={style}
+					onChange={type === "checkbox" ? onChange : null}
+					placeholder={placeholder}
+					checkboxStatus={checkboxStatus}
+				/>
+				{iconRightUrl && (
+					<img
+						className='inputIconRight'
+						src={iconRightUrl}
+						alt={iconRightAlt}
+					/>
+				)}
+			</div>
+		</>
+	)
+}
+
+export default Input
---
+++ src/pages/splash/Splash.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import "./splash.css"
+import SplashhomeBanner from "../../assets/images/homeBanner.png"
+import Button from "../../components/button/Button"
+import { useNavigate } from "react-router-dom"
+const Splash = () => {
+	const navigate = useNavigate()
+
+	return (
+		<div className='splashWrapper'>
+			<div className='splashContainer'>
+				<div className='splashBanner'>
+					<img
+						className='bannerImage'
+						src={SplashhomeBanner}
+						alt='Day Task Banner'
+					/>
+				</div>
+				<div className='splashContent'>
+					<p className='title'>
+						Manage your Task with <span>DayTask</span>
+					</p>
+					<Button
+						className='splashButton mainBtn solid'
+						type='button'
+						value="Let's Start"
+						onClick={() => {
+							navigate("/authentication")
+						}}
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Splash
